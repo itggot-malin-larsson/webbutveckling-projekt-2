e1e2ff0f3fb3f9a8e37a074db4d135f98444f3e5
itggot-malin-larsson <malin.larsson@elev.ga.ntig.se>
2018-12-20 13:23:43 +0100

    Skapade html och css. Gjorde första sidan, lade till bilder.

---
+++ site/js/main.js
@@ -0,0 +1,27 @@
+function showbutton(event){
+let Show= document.querySelector(".buttonbox")
+Show.classList.add("showbutton")}
+
+function coverup(event){
+let cover= document.querySelector(".cover")
+cover.classList.add("activatecover")  
+}
+ 
+function changen(event){
+let change= document.getElementById("n")
+change.classList.add("changen")
+}
+
+function changet(event){
+let change= document.getElementById("t")
+change.classList.add("changet")
+}
+
+function changea(event){
+let change= document.getElementById("a")
+change.classList.add("changea")
+}
+
+ 
+
+   
